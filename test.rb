7ba264eca8c575448d0fcb27b911ded872d694b2
mariomac <mariomac@gmail.com>
2014-11-28 18:42:22 +0100

    improved test script

--- test.rb
+++ test.rb
@@ -3,17 +3,26 @@
 require 'FileUtils'
 
 OUT_DIR = "/tmp/MongoALtest"
+
+GRAMMAR_NAME = "MongoAL"
 GRAMMAR_FILE = "./grammar/MongoAL.g4"
 
-`export CLASSPATH=".:/usr/local/lib/antlr-4.4-complete.jar:$CLASSPATH"`
-`alias antlr4=''`
-`alias grun='java org.antlr.v4.runtime.misc.TestRig'`
+
+START_RULE = "query"
+TESTS_DIR ="./tests"
+
+COLOR_FILE="\033[38;5;1m"
+COLOR_TITLE="\033[38;5;3m"
+COLOR_TESTBLOCK="\033[38;5;7m"
+COLOR_TESTRESULT="\033[38;5;4m"
+
+ENV["CLASSPATH"] = ".:#{OUT_DIR}:/usr/local/lib/antlr-4.4-complete.jar:$CLASSPATH"
 
 
 if ARGV[0]
   testfiles = [ARGV[0]]
 else
-  testfiles = Dir["./tests/*"];
+  testfiles = Dir["#{TESTS_DIR}/*"];
 end
 
 begin
@@ -22,19 +31,25 @@ rescue Errno::EEXIST => e
   puts "Directory #{OUT_DIR} already exists. Continuing..."
 end
 
-#puts "Compiling Grammar file #{GRAMMAR_FILE}"
-#puts `echo "#{OUT_DIR} y #{GRAMMAR_FILE}"`
-#puts `java -jar /usr/local/lib/antlr-4.4-complete.jar -o #{OUT_DIR} #{GRAMMAR_FILE}`
+puts "Compiling Grammar file #{GRAMMAR_FILE}"
+puts `java -jar /usr/local/lib/antlr-4.4-complete.jar -o #{OUT_DIR} #{GRAMMAR_FILE}`
+puts `javac -sourcepath #{OUT_DIR}/grammar -d #{OUT_DIR} #{OUT_DIR}/grammar/#{GRAMMAR_NAME}*.java`
 
 for file in testfiles do
-  puts "*** Analysing #{file}..."
+  puts "#{COLOR_FILE}*** Analysing #{file}..."
   fileText = File.open(file).read
   fileText.split("/*").each do |testBlock|
     testBlock = testBlock.split("*/");
     comment = testBlock[0].to_s.strip;
     if comment.length > 0
-      puts "--- New test: #{comment}"
-      puts testBlock[1].to_s.strip
+      testString = testBlock[1].to_s.strip
+      puts "#{COLOR_TITLE}--- New test: #{comment}"
+      puts "#{COLOR_TESTBLOCK}#{testString}"
+      print COLOR_TESTRESULT
+      puts `echo "#{testString}" | java org.antlr.v4.runtime.misc.TestRig #{GRAMMAR_NAME} #{START_RULE} -gui`
     end
   end
 end
+
+# reset console colors
+`reset`
